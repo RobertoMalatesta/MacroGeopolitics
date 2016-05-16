7836538bd3c4787c5cc5762c26e98e92c8ebdf1f
Daniel Politte <danpolitte@gmail.com>
2016-05-16 12:00:21 -0500

    First commit. Skeleton.

---
+++ src/GenericCell.py
@@ -0,0 +1,17 @@
+'''
+Created on May 16, 2016
+
+@author: dvpolitte
+'''
+
+class GenericCell(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ src/GenericGridDriver.py
@@ -0,0 +1,17 @@
+'''
+Created on May 16, 2016
+
+@author: dvpolitte
+'''
+
+class GenericGridDriver(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
